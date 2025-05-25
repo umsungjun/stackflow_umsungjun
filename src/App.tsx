25e389dc746164edf91acc294eb1dbeef94a43ad
umsungjun <umseongjun@naver.com>
2025-05-25 19:24:40 +0900

    feat: Initial commit With Tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
